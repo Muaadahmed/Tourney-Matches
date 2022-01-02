95db0d3c1ce4bf7404e803b7798875c231349510
Muaad Ahmed <muaad.abdi.ahmed@gmail.com>
2022-01-02 18:13:33 +0000

    Import helper functions to PlayerList component console.log to make sure it outputs data correctly

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,8 +2,13 @@ import React from "react"; //optional
 import Player from "./Player";
 import playerData from "../data/playerData";
 import matchData from "../data/matchData";
+import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
 
 function PlayerList() {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  console.log(parsedPlayerData);
+
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
